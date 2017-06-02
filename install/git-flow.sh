4c409863c9f48e8dae45c680c06ca18f714ad967
Dmitry Somov <smv.dmitry@gmail.com>
2017-06-02 21:34:14 +0300

    Initial commit

---
+++ install/git-flow.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -OL https://raw.github.com/nvie/gitflow/develop/contrib/gitflow-installer.sh
+chmod +x gitflow-installer.sh
+sudo ./gitflow-installer.sh
+sudo rm ./gitflow-installer.sh
+sudo rm -rf ./gitflow
